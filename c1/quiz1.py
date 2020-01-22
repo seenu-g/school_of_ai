c8f30218a7d75989324e9dc4089448495da945c9
G <gsvasan@yahoo.com>
2020-01-22 16:56:47 +0530

    assignment

---
+++ c1/quiz1.py
@@ -0,0 +1,19 @@
+#With what integer value shall x be initialized as, such that Final Value of x is 268.5? 
+#Please mention the answer in INT value without decimal
+x = 37 #2 # Initial Value of x
+print(type(x)) # Prints "<class 'int'>"
+print(x)       # Prints "3"
+print(x + 1)   # Addition; prints "4"
+print(x - 1)   # Subtraction; prints "2"
+print(x * 2)   # Multiplication; prints "6"
+print(x ** 2)  # Exponentiation; prints "9"
+x += 1
+print(x)  # Prints "4"
+x *= 2
+print(x)  # Prints "8"
+y = 2.5
+print(type(y)) # Prints "<class 'float'>"
+print(y, y + 1, y * 2, y ** 2) # Prints "2.5 3.5 5.0 6.25"
+
+x = x * y + x + y # Final value of x
+print("Final Value of x is:", x)
